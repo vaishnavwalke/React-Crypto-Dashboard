0a7e36646177e6f65af75bedad5ff9cbcacc8bc8
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2023-10-01 12:54:05 +0530

    3 files modified

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,21 @@
-import { Box } from '@chakra-ui/react';
-
+import { HStack, Icon, Stack, Text } from '@chakra-ui/react';
+import { RxDashboard } from 'react-icons/rx';
 const Sidenav = () => {
-  return <Box as="h2">Sidenav</Box>;
+  const navLinks = [
+    { icon: RxDashboard, text: 'Dashboard', link: '/' },
+    { icon: RxDashboard, text: 'Transactions', link: '/transactions' },
+  ];
+
+  return (
+    <Stack>
+      {navLinks.map((nav) => {
+        console.log(nav.text);
+        <HStack key={nav.text}>
+          <Icon as={nav.icon} />
+          <Text>{nav.text} </Text>
+        </HStack>;
+      })}
+    </Stack>
+  );
 };
 export default Sidenav;
