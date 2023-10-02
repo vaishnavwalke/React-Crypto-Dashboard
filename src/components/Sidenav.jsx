41b166e1af1e950c8ee8417b0bf80491914932c3
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2023-10-02 10:27:10 +0530

    Sidenav completed

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,7 @@
-import { HStack, Icon, Stack, Text } from '@chakra-ui/react';
+import { Box, HStack, Heading, Icon, Stack, Text } from '@chakra-ui/react';
 import { RxDashboard } from 'react-icons/rx';
 import { BsArrowDownUp } from 'react-icons/bs';
+import { BiSupport } from 'react-icons/bi';
 const Sidenav = () => {
   const navLinks = [
     { icon: RxDashboard, text: 'Dashboard', link: '/' },
@@ -8,14 +9,52 @@ const Sidenav = () => {
   ];
 
   return (
-    <Stack>
-      {navLinks.map((nav) => (
-        //console.log(nav.text)
-        <HStack key={nav.text}>
-          <Icon as={nav.icon} />
-          <Text>{nav.text} </Text>
+    <Stack justify="space-between" boxShadow="lg" maxW="16rem" h="100vh">
+      <Box>
+        <Heading textAlign="center" fontSize="20px" as="h1" pt="3.5rem">
+          @DoSomeCoding
+        </Heading>
+
+        <Box mt="6" mx="3">
+          {navLinks.map((nav) => (
+            <HStack
+              borderRadius="10px"
+              mx="3"
+              key={nav.text}
+              py="3"
+              px="4"
+              _hover={{
+                bg: '#F3F3F7',
+                color: '#171717',
+              }}
+              color="#797E82"
+            >
+              <Icon as={nav.icon} />
+              <Text fontSize="14px" fontWeight="medium">
+                {nav.text}{' '}
+              </Text>
+            </HStack>
+          ))}
+        </Box>
+      </Box>
+      <Box mt="6" mx="3" mb="6">
+        <HStack
+          borderRadius="10px"
+          mx="3"
+          py="3"
+          px="4"
+          _hover={{
+            bg: '#F3F3F7',
+            color: '#171717',
+          }}
+          color="#797E82"
+        >
+          <Icon as={BiSupport} />
+          <Text fontSize="14px" fontWeight="medium">
+            Support
+          </Text>
         </HStack>
-      ))}
+      </Box>
     </Stack>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import '@fontsource/ubuntu'; // Defaults to weight 400
+import '@fontsource/ubuntu';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/ubuntu/500.css';
+import '@fontsource/ubuntu/700.css';
 import { theme } from './theme/index.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
