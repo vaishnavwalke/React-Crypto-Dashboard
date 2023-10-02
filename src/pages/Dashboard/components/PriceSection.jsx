4a84a11b08be099369a38ff5955936cb50bfa995
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2023-10-02 17:55:36 +0530

    1 file modified

--- src/pages/Dashboard/components/PriceSection.jsx
+++ src/pages/Dashboard/components/PriceSection.jsx
@@ -1,8 +1,25 @@
-import { Button, Card, Flex, HStack, Icon, Stack, Tag, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Card,
+  Flex,
+  HStack,
+  Icon,
+  Image,
+  Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tag,
+  Text,
+} from '@chakra-ui/react';
 import { CustomCard } from '../../../chakra/CustomCard';
 import { BsArrowUpRight } from 'react-icons/bs';
-import {AiFillPlusCircle,AiFillMinusCircle} from 'react-icons/ai';
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
 const PriceSection = () => {
+  const timestamps = ['7:15 PM', '7:55 PM', '8:55 PM', '9:55 PM', '10:55 PM'];
+  // console.log(timestamps);
   return (
     <CustomCard>
       <Flex justify="space-between" align="start">
@@ -39,6 +56,35 @@ const PriceSection = () => {
           <Button leftIcon={<Icon as={AiFillMinusCircle} />}>Sell </Button>
         </HStack>
       </Flex>
+      <Tabs variant="soft-rounded" >
+        <Flex justify="end">
+          <TabList bg="black.5" p="3px">
+            {['1H', '1D', '1W', '1M'].map((tab) => (
+              <Tab _selected={{
+                bg:"white"
+              }} fontSize="sm" p="6px" borderRadius="4">
+                {tab}
+              </Tab>
+            ))}
+          </TabList>
+        </Flex>
+
+        <TabPanels>
+          <TabPanel>
+            <Image src="/graph.svg" mt="3rem" />
+            <HStack justify="space-between">
+              {timestamps.map((timestamp) => (
+                <Text key={timestamp} fontSize="sm" color="black.80">
+                  {timestamp}
+                </Text>
+              ))}
+            </HStack>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </CustomCard>
   );
 };
