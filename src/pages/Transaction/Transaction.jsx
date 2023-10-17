5c9625e7a7498acf909da5836277558f87c3f465
vaishnavwalke <vaishnavwalke2@gmail.com>
2023-10-17 16:12:30 +0530

    transaction page added

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -4,14 +4,52 @@ import { Button } from "@chakra-ui/react";
 import { Flex } from "@chakra-ui/react";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Icon } from "@chakra-ui/react";
+import { Tabs } from "@chakra-ui/react";
+import { Tab, TabList } from "@chakra-ui/react";
+import { TabPanels } from "@chakra-ui/react";
+import { TabPanel } from "@chakra-ui/react";
+import { Tag } from "@chakra-ui/react";
+import TransactionTable from "./components/TransactionTable";
 //transaction
 const TransactionPage = () => {
+  const tabs = [
+    { name: "All", count: 349 },
+    { name: "Deposit", count: 114 },
+    { name: "Withdrawal", count: 55 },
+    { name: "Trade", count: 50 },
+  ];
+
   return (
     <DashboardLayout>
-      <Flex justify="end" mt="6">
+      <Flex justify="end" mt="6" mb="3">
         <Button leftIcon={<Icon as={AiOutlineDownload} />}>Export CSV</Button>
       </Flex>
-      <Card>TransactionPage</Card>
+      <Card>
+        <Tabs>
+          <TabList pt="4">
+            {tabs.map((tab) => (
+              <Tab key={tab.name} display="flex" gap="2">
+                {tab.name}{" "}
+                <Tag colorScheme="gray" borderRadius="full">
+                  {tab.count}
+                </Tag>
+              </Tab>
+            ))}
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
     </DashboardLayout>
   );
 };
---
+++ src/pages/Transaction/components/TransactionTable.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+const TransactionTable = () => {
+  const tableData = [
+    {
+      id: "HD82NA2H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "INR Deposit",
+        tag: "E-Transfer",
+      },
+      amount: "+₹81,123",
+      status: "pending",
+    },
+    {
+      id: "HD82NA4H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "INR Widthdraw",
+        tag: "Wire Transfer",
+      },
+      amount: "-₹55,123",
+      status: "processing",
+    },
+    {
+      id: "HD82NA5H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Buy",
+        tag: "BTC",
+      },
+      amount: "12.0554484 BTC",
+      status: "cancelled",
+    },
+    {
+      id: "HD82NA6H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Sell",
+        tag: "BTC",
+      },
+      amount: "-2.0554484 BTC",
+      status: "completed",
+    },
+    {
+      id: "HD82NA7H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Deposit",
+      },
+      amount: "+15.5000000",
+      status: "pending",
+    },
+    {
+      id: "HD82NA8H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Widthdraw",
+      },
+      amount: "-5.05555544",
+      status: "completed",
+    },
+  ];
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Date & Time</Th>
+            <Th>Type</Th>
+            <Th>Amount</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {tableData.map((data) => (
+            <Tr key={data.id}>
+              <Td>{data.id}</Td>
+              <Td>
+                <Stack>
+                  <Text>{data.date}</Text>
+                  <Text>{data.time}</Text>
+                </Stack>
+              </Td>
+              <Td>
+                <Stack>
+                  <Text>{data.type.name}</Text>
+                  <Text>{data.type.tag}</Text>
+                </Stack>
+              </Td>
+              <Td>{data.amount}</Td>
+              <Td>{data.status}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TransactionTable;
