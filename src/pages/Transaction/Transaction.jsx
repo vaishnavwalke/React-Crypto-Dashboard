932f2be9d8680844fa35bee07ea122f19b5ff328
vaishnavwalke <vaishnavwalke2@gmail.com>
2023-10-17 11:23:15 +0530

    update3

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -1,9 +1,16 @@
 import { Card } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
+import { Button } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Icon } from "@chakra-ui/react";
 
 const TransactionPage = () => {
   return (
     <DashboardLayout>
+      <Flex justify="end" mt="6">
+        <Button leftIcon={<Icon as={AiOutlineDownload} />}>Export CSV</Button>
+      </Flex>
       <Card>TransactionPage</Card>
     </DashboardLayout>
   );
