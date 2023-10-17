e39205de98a625489433faf820baff97d01eb838
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2023-10-17 10:17:34 +0530

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Dashboard from './pages/Dashboard/Dashboard';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Transactions from './pages/Dashboard/components/Transactions';
+import Support from './pages/Support/Support';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/transactions',
+    element: <Transactions />,
+  },
+  {
+    path: '/support',
+    element: <Support />,
+  },
+]);
 function App() {
   return (
     <div>
-      <Dashboard />
+      <RouterProvider router={router} />
     </div>
   );
 }
